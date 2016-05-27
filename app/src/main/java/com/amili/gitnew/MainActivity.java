d1ecffff5d4ca834c993419a501d9a6354428da9
chr <sqzychr321@163.com>
2016-05-27 11:59:46 +0800

    双击后退键退出层序

--- app/src/main/java/com/amili/gitnew/MainActivity.java
+++ app/src/main/java/com/amili/gitnew/MainActivity.java
@@ -2,6 +2,7 @@ package com.amili.gitnew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private long lastTime;
+    @Override
+    public void onBackPressed() {
+        long ct=System.currentTimeMillis();
+        super.onBackPressed();
+        if(ct-lastTime>200){
+            Toast.makeText(this,"点击后退",Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(this,"再次点击后退",Toast.LENGTH_LONG).show();
+        }
     }
 }
